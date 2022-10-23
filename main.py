028c51f37d9db9b0bc07411236813c6ec1fa47e2
Alexander Donner <donneralexander22@gmx.at>
2022-10-23 23:30:53 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+food = [
+    'Pizza',
+    'Pasta',
+    'Schnitzel',
+    'Kebap',
+    'Sushi',
+    'Burger',
+]
+
+# random.randint(1, 5) --> random number between 1 and 5
+# random.choice(array) --> random item in this array
+
+selected = random.choice(food) # randomly choose a friend
+
+print('Who should I facetime today?')
+print(selected)
